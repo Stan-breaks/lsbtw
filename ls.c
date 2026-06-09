c58d38302bc6d5e71dbabbc5009f8ccf38d8fde5
Stan-breaks <stanleymwendwa03@gmail.com>
2026-06-09 16:57:02 +0300

    feat: error handling

--- ls.c
+++ ls.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <dirent.h>
+
 int main (int argc, char *argv[]){
   const char *path = (argc > 1)?argv[1]:".";
-  printf("%s\n",path);
+  DIR *dir = opendir(path);
+  if (!dir){
+    perror("opendir");
+    return 1;
+  }
   return 0;
 }
