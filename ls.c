ec2a05a08953308154c56f2167fa3da3d81ab224
Stan-breaks <stanleymwendwa03@gmail.com>
2026-06-15 16:21:28 +0300

    feat: formatting

--- ls.c
+++ ls.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
 #include <dirent.h>
+#include <stdio.h>
 
-int main (int argc, char *argv[]){
-  const char *path = (argc > 1)?argv[1]:".";
+int main(int argc, char *argv[]) {
+  const char *path = (argc > 1) ? argv[1] : ".";
   DIR *dir = opendir(path);
-  if (!dir){
+  if (!dir) {
     perror("opendir");
     return 1;
   }
+  struct dirent *entry;
+  entry = readdir(dir);
+  printf("%s\n", entry->d_name);
   return 0;
 }
