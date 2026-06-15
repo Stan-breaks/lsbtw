15d7f2fe604e1d548860f98339f93a60bcae2095
Stan-breaks <stanleymwendwa03@gmail.com>
2026-06-15 16:57:00 +0300

    feat: flags

--- ls.c
+++ ls.c
@@ -1,8 +1,21 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <unistd.h>
 
+int show_all = 0;
 int main(int argc, char *argv[]) {
-  const char *path = (argc > 1) ? argv[1] : ".";
+  int opt;
+  while ((opt = getopt(argc, argv, "a")) != -1) {
+    switch (opt) {
+    case 'a':
+      show_all = 1;
+      break;
+    default:
+      fprintf(stderr, "usage: %s [-a] [path]\n", argv[0]);
+      return 1;
+    }
+  }
+  const char *path = (optind < argc) ? argv[optind] : ".";
   DIR *dir = opendir(path);
   if (!dir) {
     perror("opendir");
@@ -10,6 +23,11 @@ int main(int argc, char *argv[]) {
   }
   struct dirent *entry;
   entry = readdir(dir);
-  printf("%s\n", entry->d_name);
+  while ((entry = readdir(dir)) != NULL) {
+    if (!show_all && entry->d_name[0] == '.')
+      continue;
+    printf("%s\n", entry->d_name);
+  }
+  closedir(dir);
   return 0;
 }
